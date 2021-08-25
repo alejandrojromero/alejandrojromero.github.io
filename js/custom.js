ea2415dfcaef0f54d0788848e769dff553cd0142
Alejandro Romero <alejandro_romero@brown.edu>
2021-08-24 19:03:32 -0700

    add progressbar

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,7 @@ var wh = win.height();
 $(document).ready(function () {
     fadeInPage();
     sidebar();
+    scrollProgressBar();
 });
 
 function scrollSmoothTo(elementId) {
@@ -22,6 +23,42 @@ function scrollSmoothTo(elementId) {
         behavior: 'smooth'
     });
 }
+
+
+//progress bar 
+function scrollProgressBar() {
+    var getMax = function () {
+      return $(document).height() - $(window).height();
+    };
+  
+    var getValue = function () {
+      return $(window).scrollTop();
+    };
+  
+    var progressBar = $(".progress-bar"),
+      max = getMax(),
+      value,
+      width;
+  
+    var getWidth = function () {
+      // Calculate width in percentage
+      value = getValue();
+      width = (value / max) * 100;
+      width = width + "%";
+      return width;
+    };
+  
+    var setWidth = function () {
+      progressBar.css({ width: getWidth() });
+    };
+  
+    $(document).on("scroll", setWidth);
+    $(window).on("resize", function () {
+      // Need to reset max
+      max = getMax();
+      setWidth();
+    });
+  }
 
 /** Change tag passing anchor point */
 function sidebar() {
