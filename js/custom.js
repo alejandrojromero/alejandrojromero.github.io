10a9eddae980733c4ac46f1b6fa98a6f30198ddf
Alejandro Romero <alejandro_romero@brown.edu>
2021-08-24 19:13:45 -0700

    scroll behavior

--- js/custom.js
+++ js/custom.js
@@ -25,40 +25,7 @@ function scrollSmoothTo(elementId) {
 }
 
 
-//progress bar 
-function scrollProgressBar() {
-    var getMax = function () {
-      return $(document).height() - $(window).height();
-    };
-  
-    var getValue = function () {
-      return $(window).scrollTop();
-    };
-  
-    var progressBar = $(".progress-bar"),
-      max = getMax(),
-      value,
-      width;
-  
-    var getWidth = function () {
-      // Calculate width in percentage
-      value = getValue();
-      width = (value / max) * 100;
-      width = width + "%";
-      return width;
-    };
-  
-    var setWidth = function () {
-      progressBar.css({ width: getWidth() });
-    };
-  
-    $(document).on("scroll", setWidth);
-    $(window).on("resize", function () {
-      // Need to reset max
-      max = getMax();
-      setWidth();
-    });
-  }
+
 
 /** Change tag passing anchor point */
 function sidebar() {
